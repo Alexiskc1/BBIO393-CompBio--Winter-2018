547a85900072f60f3e7fecfe88d4a9cfe3f2b977
Alexiskc1 <36253936+Alexiskc1@users.noreply.github.com>
2018-03-08 09:51:25 -0800

    file via upload
    code for project_extinctions

---
+++ mass_extinctions_project_2.py
@@ -0,0 +1,16 @@
+ages = [{"Start year":411.0,"Era":"Devonian","Age":"Paleozoic"},
+  {"Start year":443.7.0,"Era":"Ordovician","Age":"Paleozoic"}]
+
+Year = 420
+Last_age = None
+For age in ages:
+	If last_age == None:
+		Last_age = age
+
+	If year < age["start year"]:
+		#that means that this event is in the *last* age
+		Correct_age = last_age
+		Break
+
+Era = correct_age["era"]
+Period = correct_age["age"]
